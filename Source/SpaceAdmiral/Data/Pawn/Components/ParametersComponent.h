02011eb1b8d46f5ed56eac788d1b7eea68cf3251
bikekun <potoshop1@gmail.com>
2016-07-16 11:35:59 +0300

    add parameters component

---
+++ Source/SpaceAdmiral/Data/Pawn/Components/ParametersComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Data/Pawn/Components/HealthComponent.h"
+#include "ParametersComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEADMIRAL_API UParametersComponent : public UHealthComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
